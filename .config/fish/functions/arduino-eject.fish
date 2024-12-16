7b52e2fd8d50ae4e5d1268fa5b3062c2618bc6a9
TomAintAround <tcorreia2005@proton.me>
2024-12-16 02:51:39 +0000

    feat(fish): add arduino-cli upload function

---
+++ .config/fish/functions/arduino-eject.fish
@@ -0,0 +1,9 @@
+function arduino-eject --description="Compile and upload arduino file into Arduino and open the serial monitor"
+    set file $argv[1]
+    set usb $argv[2]
+    set fqbn $argv[3]
+
+    arduino-cli compile --fqbn $fqbn $file
+    and arduino-cli upload -p $usb --fqbn $fqbn $file
+    and arduino-cli monitor -p $usb --fqbn $fqbn
+end
