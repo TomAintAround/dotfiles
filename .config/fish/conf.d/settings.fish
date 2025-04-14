9a0e595eaca4f7ac5f4086097222e73c5fc9d43e
TomAintAround <tcorreia2005@proton.me>
2025-04-14 21:54:03 +0100

    feat(fish): add keybind for neovim

--- .config/fish/conf.d/settings.fish
+++ .config/fish/conf.d/settings.fish
@@ -24,7 +24,7 @@ set fish_greeting ""
 # Vi Mode
 fish_vi_key_bindings
 set fish_cursor_default block
-set fish_cursor_insert line 
+set fish_cursor_insert line
 set fish_cursor_replace_one underscore
 set fish_cursor_visual block
 
@@ -32,6 +32,9 @@ set fish_cursor_visual block
 bind \cf 'commandline -f kill-whole-line && lf && commandline -f execute'
 bind -M insert \ef 'commandline -f kill-whole-line && lf && commandline -f execute'
 bind -M visual \ef 'commandline -f kill-whole-line && lf && commandline -f execute'
+bind \ce 'commandline -f kill-whole-line && nvim . && commandline -f execute'
+bind -M insert \ce 'commandline -f kill-whole-line && nvim . && commandline -f execute'
+bind -M visual \ce 'commandline -f kill-whole-line && nvim . && commandline -f execute'
 
 # Done Plugin
 set -a __done_exclude '^pgrep'
